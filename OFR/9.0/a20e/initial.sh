4ed0b676f52af1d20ab7a96e1a857003e0958c18
Enzo Vieira | YukoSky <daffetyxd@gmail.com>
2020-11-22 21:40:28 -0300

    [OFR] A20e
    Signed-off-by: Enzo Vieira | YukoSky <daffetyxd@gmail.com>

--- OFR/9.0/a20e/initial.sh
+++ OFR/9.0/a20e/initial.sh
@@ -23,7 +23,7 @@ mkdir -p "$THIS_DIR/OrangeFox/"
 cd "$THIS_DIR/OrangeFox/"
 repo init --depth=1 -u https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0
 repo sync --no-repo-verify -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j16
-git clone --depth 1 https://github.com/a2XX-dev/android_device_samsung_a20e -b fox-9.0 device/samsung/a20e
+git clone --depth 1 https://github.com/a2XX-dev/android_device_samsung_a20e -b fox-9.0-new device/samsung/a20e
 mv device/samsung/a20e/*.sh .
 chmod +x build_ofox.sh
 
